70933338362a8b8bb6fe1576f45529211c9d7ffd
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2024-04-30 15:48:44 +0500

    Updated main.cpp According to the new functions, and made it to fit the GUI

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-#include <iostream>
-#include "functions.cpp"
+#include<SFML\Graphics.hpp>
+#include"Functions.cpp"
 using namespace std;
+using namespace sf;
 
-int main(){
-    bombCheck C;
-    C.play();
+int main() {
+	RenderWindow window(VideoMode(800, 600), "Title");
+	Event event;
+
+	Game play;
+	while (window.isOpen()) {
+		while (window.pollEvent(event)) {
+			if (event.type == Event::Closed) {
+				window.close();
+			}
+			window.clear();
+			play.gamecontroller(&window);
+			if (play.gameRunner == false) {
+				window.close();
+				break;
+			}
+			window.display();
+		}
+	}
 }
