95e81271b70a824e17ccc8ddc464153046bec1dd
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2024-05-03 19:12:59 +0500

    Update main.cpp
    - Added Pause() function for pause game functionality
    - Fixed win() and lose() functions to fit the Pause function

--- main.cpp
+++ main.cpp
@@ -1,24 +1,43 @@
 #include<SFML\Graphics.hpp>
+#include<Windows.h>
 #include"Functions.cpp"
 using namespace std;
 using namespace sf;
 
+void Pause(bool game_pause, RenderWindow *window, Game *play, Event event) {
+	while (game_pause) {
+		window->clear();
+		play->Print(window);
+		window->display();
+		if (event.type == Event::Closed) {
+			window->close();
+		}
+		if (Keyboard::isKeyPressed(Keyboard::P)) {
+			game_pause = false;
+			return;
+		}
+	}
+}
+
 int main() {
-	RenderWindow window(VideoMode(800, 600), "Title");
+	RenderWindow window(VideoMode(340, 340), "BOMBBRUSHER™");
 	Event event;
 
-	Game play;
+	Game *play = new Game;
+
 	while (window.isOpen()) {
 		while (window.pollEvent(event)) {
 			if (event.type == Event::Closed) {
 				window.close();
 			}
 			window.clear();
-			play.gamecontroller(&window);
-			if (play.gameRunner == false) {
-				window.close();
-				break;
-			}
+				play->gamecontroller(&window);
+				if (play->gameRunner == false) {
+					Pause(true, &window, play, event);
+					delete play;
+					play = new Game;
+					play->gameRunner = true;
+				}
 			window.display();
 		}
 	}
